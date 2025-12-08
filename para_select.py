f6bf754312647a9eba1c2771529a111f11a58fb6
Minsik <minsik@stanford.edu>
2025-12-08 11:16:57 -0800

    Bugfix

--- para_select.py
+++ para_select.py
@@ -40,8 +40,7 @@ class ParagraphSelector(BertPreTrainedModel):
             print("Logits: ", logits)
             loss = loss_fct(logits.squeeze(-1), labels)
         print("Loss: ", loss.item())
-        print("Outputs: ", outputs)
-        outputs = (loss,) + (logits,) + outputs
+        outputs = (loss,) + (logits,)
         return outputs  # (loss,), (binary_logits), logits_bert, (hidden_states), (attentions)
 
 
