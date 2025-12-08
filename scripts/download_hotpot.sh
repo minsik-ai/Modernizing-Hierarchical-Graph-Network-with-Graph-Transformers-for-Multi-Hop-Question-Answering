1b8e8829a77eb5fc303cef5cabd8cbc0c89602d8
Minsik <minsik@stanford.edu>
2025-12-08 04:25:15 -0800

    Temporary setting for eval

--- scripts/download_hotpot.sh
+++ scripts/download_hotpot.sh
@@ -1,3 +1,4 @@
 mkdir /nlp/scr/minsik/hgn_dgl/data
 cd /nlp/scr/minsik/hgn_dgl/data
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
+# wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json	
