c13b3740d228a18cc4906a40955c3070f7ce01f7
Minsik <minsik@stanford.edu>
2025-12-10 06:52:34 -0800

    No Gated Attention

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --no_bi_attention --task train_model $1
+python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --no_gated_attention --task train_model $1
