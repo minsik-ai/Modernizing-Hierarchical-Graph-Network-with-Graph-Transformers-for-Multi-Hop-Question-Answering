f1773af5ed898d967faf7e244e4b704bdcbc918c
Minsik <minsik@stanford.edu>
2025-12-10 09:29:44 -0800

    Skip weight 1.0

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --task train_model $1
+python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --transformer_skip_weight 1.0 --task train_model $1
