b4d9b31b023ef9439c0caefac1b8dc42106ed0e6
Minsik <minsik@stanford.edu>
2025-12-09 23:45:20 -0800

    Roberta encoding bugfix

--- model.py
+++ model.py
@@ -296,8 +296,12 @@ class ContextEncoder(RobertaPreTrainedModel):
         _, self.model_class, _ = MODEL_CLASSES[args.model_type]
         self.encoder = self.model_class.from_pretrained(self.args.model_name_or_path, config=self.config)
 
-    def forward(self, input_ids, attention_mask, token_type_ids):
-        encoded_out = self.encoder(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+    def forward(self, input_ids, attention_mask, token_type_ids=None):
+        # RoBERTa doesn't use token_type_ids, only pass if model supports it
+        if 'roberta' in self.args.model_type.lower():
+            encoded_out = self.encoder(input_ids, attention_mask=attention_mask)
+        else:
+            encoded_out = self.encoder(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         return encoded_out
 
 
