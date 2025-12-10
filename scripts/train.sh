515580f729f1985b264126f9e87388b14d6ed270
Minsik <minsik@stanford.edu>
2025-12-10 07:19:27 -0800

    Num transformer layers 2

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --num_gat_layers 2 --task train_model $1
+python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --num_transformer_layers 2 --task train_model $1
