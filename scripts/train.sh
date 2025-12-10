2271ad46353ecd482254704ce92b470356daf635
Minsik <minsik@stanford.edu>
2025-12-10 08:09:25 -0800

    Freeze the encoder

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --task train_model $1
+python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --num_train_epochs 1 --freeze_encoder --task train_model $1
