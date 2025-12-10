97579ab0186beb6000244f7adee070289782dee6
Minsik <minsik@stanford.edu>
2025-12-10 07:48:51 -0800

    No node type embed

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --no_entity_nodes --no_sentence_nodes --num_train_epochs 1 --task train_model $1
+python main.py --do_train --train_batch_size 1 --eval_batch_size 1 --gradient_accumulation_steps 32 --no_node_type_embed --num_train_epochs 1 --task train_model $1
