19e964cfb398b3571f0644c409ca9e827f7800cf
Minsik <minsik@stanford.edu>
2025-12-10 06:44:07 -0800

    Num attention heads 1

--- model.py
+++ model.py
@@ -288,7 +288,7 @@ class BiAttention(nn.Module):
         return query, G
 
 
-class ContextEncoder(BertPreTrainedModel):
+class ContextEncoder(RobertaPreTrainedModel):
     def __init__(self, args, config):
         super().__init__(config)
         self.args = args
